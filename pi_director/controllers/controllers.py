90e8f58c24608c503697e9d491ff77b5b46972ba
Peter Grace <pete.grace@gmail.com>
2015-09-08 17:20:15 +0000

    Order Pis in list by lastseen, showing non-communicating pis at the top

--- pi_director/controllers/controllers.py
+++ pi_director/controllers/controllers.py
@@ -4,6 +4,6 @@ from pi_director.models.models import (
     )
 
 def get_pis():
-    PiList=DBSession.query(MyModel).filter(MyModel.uuid!="default").all()
+    PiList=DBSession.query(MyModel).filter(MyModel.uuid!="default").order_by(MyModel.lastseen).all()
     return PiList
 
