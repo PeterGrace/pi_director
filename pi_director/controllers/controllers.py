d1a24060337afe94c61fe3c6729ea0712a69bfa9
Peter Grace <pete.grace@gmail.com>
2015-09-08 20:55:47 +0000

    descending makes a lot more sense for us at the moment

--- pi_director/controllers/controllers.py
+++ pi_director/controllers/controllers.py
@@ -2,8 +2,9 @@ from pi_director.models.models import (
     DBSession,
     MyModel,
     )
+from sqlalchemy import desc
 
 def get_pis():
-    PiList=DBSession.query(MyModel).filter(MyModel.uuid!="default").order_by(MyModel.lastseen).all()
+    PiList=DBSession.query(MyModel).filter(MyModel.uuid!="default").order_by(desc(MyModel.lastseen)).all()
     return PiList
 
