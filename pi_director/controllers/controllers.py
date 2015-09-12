3db7e6bc9685743f027af161ca422ac6ea338042
Peter Grace <pete.grace@gmail.com>
2015-09-12 21:58:05 +0000

    make the multiple tag search be inclusive

--- pi_director/controllers/controllers.py
+++ pi_director/controllers/controllers.py
@@ -1,10 +1,11 @@
 import shlex
+import logging
 from pi_director.models.models import (
     DBSession,
     MyModel,
     Tags
     )
-from sqlalchemy import desc
+from sqlalchemy import desc, and_
 
 def get_pis():
     PiList=DBSession.query(MyModel).filter(MyModel.uuid!="default").order_by(desc(MyModel.lastseen)).all()
@@ -17,11 +18,34 @@ def get_tagged_pis(tags):
     splitter.whitespace_split=True
     taglist = list(splitter)
 
+    taglist_str=""
+    for tag in taglist:
+        taglist_str+="\'{tag}\',".format(tag=tag)
+    taglist_str=taglist_str.rstrip(taglist_str[-1:])    
+
+
     tagged_pis=[]
-    PisWithTags=DBSession.query(Tags).filter(Tags.tag.in_(taglist)).all()
-    for pi in PisWithTags:
-        tagged_pis.append(pi.uuid)
-    
+
+    if (len(taglist) > 1):
+        query_str="""select t1.uuid,count(*) as ct 
+                     from Tags t1 
+                     join Tags t2 on t1.tag!=t2.tag and t1.uuid==t2.uuid
+                     where 1
+                     and t1.tag in ({taglist})
+                     and t2.tag in ({taglist})
+                     group by t1.uuid having ct >= {args};
+                  """.format(taglist=taglist_str,args=len(taglist))
+
+        result=DBSession.get_bind().execute(query_str)          
+
+        for row in result:
+            tagged_pis.append(row[0])
+    else:
+        PisWithTags=DBSession.query(Tags).filter(Tags.tag.in_(taglist)).all()
+        for pi in PisWithTags:
+            tagged_pis.append(pi.uuid)
+
+
     PiList=DBSession.query(MyModel).filter(MyModel.uuid.in_(tagged_pis)).order_by(desc(MyModel.lastseen)).all()
     return PiList
 
