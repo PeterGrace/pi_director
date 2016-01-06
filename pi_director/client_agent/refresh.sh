661c721131285ec140d48e7a3871c8838e6dc56d
Peter Grace <pete.grace@gmail.com>
2016-01-06 18:15:51 -0500

    fix refresh.sh so it will properly restart lightdm if no browser is running

--- pi_director/client_agent/refresh.sh
+++ pi_director/client_agent/refresh.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
+!/bin/bash
 
 export DISPLAY=":0"
 WID=$(xdotool search --onlyvisible --class iceweasel|head -1)
+if [[ $WID = "" ]]
+then
+        sudo service lightdm restart
+        exit 0
+fi
 xdotool windowactivate ${WID}
 xdotool key ctrl+F5
